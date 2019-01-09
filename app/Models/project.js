2f0e75433f345da81ebffa56b116efcf6fb347bc
JanSchoenfeld <jan.schoenfeld@fh-muenster.de>
2019-01-09 20:09:40 +0100

    add sprint array to project.js

--- app/Models/project.js
+++ app/Models/project.js
@@ -10,6 +10,7 @@ class Project {
         this.description = description;
         this.backlogs = [];
         this.assignedUsers = [];
+        this.sprints = [];
 
         this.addBacklog = function(newBacklog){
             this.backlogs.push(newBacklog);
@@ -17,6 +18,10 @@ class Project {
 
         this.addUser = function(newUser){
             this.assignedUsers.push(newUser);
+        }
+        
+        this.addSprint = function(newSprint){
+            this.sprints.push(newSprint);
         }
     }
 }
