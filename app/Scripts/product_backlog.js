d57980d768a28f04741d1c15ef4a75294651302d
Luc <luc.moell@gmail.com>
2019-01-16 16:56:00 +0100

    Backlog Item Link zu Modal zum bearbeiten

--- app/Scripts/product_backlog.js
+++ app/Scripts/product_backlog.js
@@ -1,67 +1,92 @@
 const fs = require('fs');
 const path = require('path');
+const { BrowserWindow } = require('electron')
+const {
+    remote
+} = require('electron');
+/*
+const Task = require('./app/Models/task');
+const Project = require('./app/Models/project');
+const Roles = require('./app/Models/roles');
+const User = require('./app/Models/user');
+const Sprint = require('./app/Models/sprint');
+const Status = require('./app/Models/status');
+const BacklogItem = require('./app/Models/backlog-item');
+
+*/
+let PROJECTS = remote.getGlobal('PROJECTS');
 const projectData = require('../logic');
-const backlogItem = require("../app/Models/backlog-item.js");
-const epicCapture = require("../app/Models/epic-capture.js");
-
-
+//const epicCapture = require("../app/Models/epic-capture.js");
 //Load JSON-File
 var jsonFile = JSON.parse(fs.readFileSync(path.join(__dirname, '/../data/pmtool.json')));
 //HTML parent <table> ID
-var table = document.getElementById('productbacklog_table');
+var table = document.getElementById("productbacklog_table");
+siteContent();
+
+function siteContent() {
+
+
 //Loop for the backlog array
-for (let i = 0; i < jsonFile.backlogs.length; i++) {
-  //decision tree
-  var newStatus;
-  if (jsonFile.backlogs[i].backlog_status.to_do) {
-    newStatus = "To do";
-  }
-  if (jsonFile.backlogs[i].backlog_status.in_progress) {
-    newStatus = "In progress";
-  }
-  if (jsonFile.backlogs[i].backlog_status.done) {
-    newStatus = "Done";
-  }
+    for (let i = 0; i < jsonFile.backlogs.length; i++) {
+        //decision tree
+        var newStatus;
+        if (jsonFile.backlogs[i].backlog_status.to_do) {
+            newStatus = "To do";
+        }
+        if (jsonFile.backlogs[i].backlog_status.in_progress) {
+            newStatus = "In progress";
+        }
+        if (jsonFile.backlogs[i].backlog_status.done) {
+            newStatus = "Done";
+        }
 
-  //New Row
-  var newTR = document.createElement("tr");
+        //New Row
+        var newTR = document.createElement("tr");
 
-  //1. Column ID
-  var newTD1 = document.createElement("td");
-  var newContent1 = document.createTextNode(jsonFile.backlogs[i].backlogId)
-  newTD1.appendChild(newContent1);
-  newTR.appendChild(newTD1);
+        //1. Column ID
+        var newTD1 = document.createElement("td");
+        var newContent1 = document.createTextNode(jsonFile.backlogs[i].backlogId);
+        newTD1.appendChild(newContent1);
+        newTR.appendChild(newTD1);
 
-  //2. Column item_description
-  var newContent2 = document.createTextNode(jsonFile.backlogs[i].description)
-  var newTD2 = document.createElement("td");
-  newTD2.appendChild(newContent2);
-  newTR.appendChild(newTD2);
+        //2. Column item_description
+        var newContent2 = document.createTextNode(jsonFile.backlogs[i].description);
+        var a = document.createElement('a');
+        a.appendChild(newContent2);
+        a.title = jsonFile.backlogs[i].description;
+        a.href = "javascript:displayEditBacklogItem("+i+")";
 
-  //3. Column priority
-  var newContent3 = document.createTextNode(jsonFile.backlogs[i].priority.priority)
-  var newTD3 = document.createElement("td");
-  newTD3.appendChild(newContent3);
-  newTR.appendChild(newTD3);
+        var newTD2 = document.createElement("td");
+        newTD2.appendChild(a);
+        newTR.appendChild(newTD2);
 
-  //4. Column estimate
-  var newContent4 = document.createTextNode(jsonFile.backlogs[i].estimated)
-  var newTD4 = document.createElement("td");
-  newTD4.appendChild(newContent4);
-  newTR.appendChild(newTD4);
+        //3. Column priority
+        var newContent3 = document.createTextNode(jsonFile.backlogs[i].priority.priority);
+        var newTD3 = document.createElement("td");
+        newTD3.appendChild(newContent3);
+        newTR.appendChild(newTD3);
 
-  //5. Column status
-  var newContent5 = document.createTextNode(newStatus)
-  var newTD5 = document.createElement("td");
-  newTD5.appendChild(newContent5);
-  newTR.appendChild(newTD5);
+        //4. Column estimate
+        var newContent4 = document.createTextNode(jsonFile.backlogs[i].estimated);
+        var newTD4 = document.createElement("td");
+        newTD4.appendChild(newContent4);
+        newTR.appendChild(newTD4);
 
-  //Add row
-  table.appendChild(newTR);
+        //5. Column status
+        var newContent5 = document.createTextNode(newStatus);
+        var newTD5 = document.createElement("td");
+        newTD5.appendChild(newContent5);
+        newTR.appendChild(newTD5);
+
+        //Add row
+        table.appendChild(newTR);
+    }
 }
 
+
 function displayChooseItem() {
-    $("#modal_chooseItem").modal("show");
+   $("#modal_chooseItem").modal("show");
+
 }
 function close_chooseItem() {
     $("#modal_chooseItem").modal("hide");
@@ -90,14 +115,14 @@ function close_addBacklogItem() {
     document.getElementById("form_addBacklogItem").reset();
     $("#modal_add_backlogItem").modal('hide');
 }
-function displayEditBacklogItem () {
-    /**
-    document.getElementById("form_edit_backlog").reset();
-    document.getElementById("edit_b_item_name").value = backlogItem.name;
-    document.getElementById("edit_b_item_description").value = backlogItem.description;
-    document.getElementById("edit_b_item_estimate_time").value = backlogItem.estimated;
-    document.getElementById("edit_b_item_assign_to_sprint").value = backlogItem.sprintId;
-*/
+function displayEditBacklogItem (i) {
+
+    document.getElementById("form_edit_BacklogItem").reset();
+    document.getElementById("edit_b_item_name").value = jsonFile.backlogs[i].name;
+    document.getElementById("edit_b_item_description").value = jsonFile.backlogs[i].description;
+    document.getElementById("edit_b_item_estimate_time").value = jsonFile.backlogs[i].estimated;
+    document.getElementById("edit_b_item_assign_to_sprint").value = jsonFile.backlogs[i].sprintId;
+
     $("#modal_edit_backlogItem").modal("show");
 }
 function saveEditBacklogItem() {
