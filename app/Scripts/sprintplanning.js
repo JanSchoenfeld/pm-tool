e1820278922fe229883e3e5610c96f9c0a0692a3
Marvin Engbert <marvin.engbert@gmail.com>
2019-01-21 10:18:35 +0100

    Sprintplanning more functions

--- app/Scripts/sprintplanning.js
+++ app/Scripts/sprintplanning.js
@@ -37,22 +37,48 @@ function siteContent() {
     row.appendChild(rightCol);
 
     var panelGroup = document.createElement('div');
-    panelGroup.className='panel-group';
+    panelGroup.className = 'panel-group';
 
-    for (let index = 0; index < 100; index++) {
+    for (let i = 0; i < project.sprints.length; i++) {
         var panel = document.createElement('div');
-        panel.className='contentLeft';
+        panel.className = 'contentLeft';
         panelGroup.appendChild(panel);
 
         var panelHead = document.createElement('div');
-        panelHead.className= 'alert alert-success';
-        panelHead.appendChild(document.createTextNode('Test '+index));
+        panelHead.className = 'alert alert-success';
+        panelHead.appendChild(document.createTextNode('Sprint ' + project.sprints[i].sprintId));
         panel.appendChild(panelHead);
 
         var panelBody = document.createElement('div');
-        panelBody.className='panel-body';
-        panelBody.appendChild(document.createTextNode('Hallo das ist ein Body von '+index));
+        panelBody.className = 'panel-body';
         panel.appendChild(panelBody);
+
+        for (let j = 0; j < project.sprints[i].backlogItemIds.length; j++) {
+            var panelBodyContent = document.createElement('div');
+            panelBodyContent.className = 'content';
+            panelBody.appendChild(panelBodyContent);
+            for (let k = 0; k < project.backlogs.length; k++) {
+                if (project.sprints[i].backlogItemIds[j].backlogID == project.backlogs[k].backlogId) {
+                    var infoDiv = document.createElement('div');
+                    panelBodyContent.appendChild(infoDiv);
+
+                    var backlog = document.createElement('div');
+                    backlog.appendChild(document.createTextNode(project.backlogs[k].title))
+                    infoDiv.appendChild(backlog);
+
+                    var actionDiv = document.createElement('div');
+                    infoDiv.appendChild(actionDiv);
+                    var editIcon = document.createElement('i');
+                    editIcon.className = "far fa-edit";
+                    var deleteIcon = document.createElement('i');
+                    deleteIcon.className = "far fa-trash-alt";
+                    actionDiv.appendChild(editIcon);
+                    actionDiv.appendChild(deleteIcon);
+                }
+
+            }
+        }
+
     }
     leftCol.appendChild(panelGroup);
     var content = document.createElement('div');
